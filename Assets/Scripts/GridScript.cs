58d4679c81be61251da1c31ba1d50dffac0ef3e5
jakob <jakob.willforss@hotmail.com>
2018-11-19 20:13:28 +0100

    First commit - Setup button grid

---
+++ Assets/Scripts/GridScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridScript : MonoBehaviour {
+
+    public GameObject mCellPrefab;
+    public int cells;
+
+    private Cell[] mCells = new Cell[1000];
+
+    void Start() {
+        Build();
+    }
+
+    public void Build() {
+        for (int i = 0; i <= cells; i++) {
+            GameObject newCell = Instantiate(mCellPrefab, transform);
+
+            mCells[i] = newCell.GetComponent<Cell>();
+        }
+    }
+}
