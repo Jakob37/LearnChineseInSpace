d68a6dcdc30260c473e974dc430eec6c7c6b5121
jakob <jakob.willforss@hotmail.com>
2018-11-19 20:54:13 +0100

    First step to parsing AnkiDroid cards into system

---
+++ Assets/Scripts/AnkiParser.cs
@@ -0,0 +1,62 @@
+﻿using Assets.Utils;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class AnkiParser : MonoBehaviour {
+
+    void Start() {
+
+        print("I am testing");
+        ParseStoryEntities("glossary");
+    }
+
+    private void ParseStoryEntities(string resource_name, string splitter = "\t") {
+    //private Dictionary<StoryBoardEntity, List<string>> ParseStoryEntities(string resource_name, string splitter = "\\|") {
+
+        //Dictionary<StoryBoardEntity, List<string>> board_entities_tmp = new Dictionary<StoryBoardEntity, List<string>>();
+
+        List<string[]> results = Utils.ParseTextToSplitList(resource_name, splitter);
+        print(results);
+
+        //for (int pos = 0; pos < results[0].Length; pos++) {
+        //    print(results[0][pos]);
+        //}
+
+        string characters = results[0][0];
+
+        characters = Regex.Replace(characters, "<.*?>", "");
+
+        string pinjing = results[0][2];
+        string meaning = results[0][3];
+
+        print(characters + ", " + pinjing + ", " + meaning);
+
+        //print(results[0][0]);
+        //foreach (string[] board_entry in board_entries) {
+
+        //    if (board_entry[0] == "" || board_entry[0].Substring(0, 1) == "#") {
+        //        continue;
+        //    }
+
+        //    if (board_entry[1] == "name") {
+        //        continue;
+        //    }
+
+        //    string board_text = board_entry[1];
+        //    if (!board_entities_tmp.ContainsKey(board_name)) {
+        //        board_entities_tmp[board_name] = new List<String>();
+        //    }
+
+        //    board_entities_tmp[board_name].Add(board_text);
+        //}
+
+        //return board_entities_tmp;
+    }
+
+
+}
---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+namespace Assets.Utils {
+    public static class Utils {
+
+        public static List<string[]> ParseTextToSplitList(string resource_name, string splitter,
+            int expected_length = 2, bool enforce_expected_length = false) {
+
+            string[] lines = ParseTextToArray(resource_name);
+            List<string[]> split_entries = new List<string[]>();
+
+            for (int i = 0; i < lines.Length; i++) {
+
+                string line = lines[i];
+                string[] values = Regex.Split(line, splitter);
+
+                if (enforce_expected_length && values.Length != expected_length) {
+                    throw new ArgumentException("Expected length " + expected_length + ", received: " + values);
+                }
+
+                split_entries.Add(values);
+            }
+            return split_entries;
+        }
+
+        public static T ParseEnum<T>(string value) {
+            return (T)Enum.Parse(typeof(T), value, true);
+        }
+
+        public static string[] ParseTextToArray(string resource_name) {
+
+            TextAsset text_resource = (TextAsset)Resources.Load(resource_name);
+            var splitFile = new string[] { "\r\n", "\r", "\n" };
+            string[] lines = text_resource.text.Split(splitFile, StringSplitOptions.None);
+            return lines;
+        }
+    }
+}
