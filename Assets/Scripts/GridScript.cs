ced811d7fa63e5ec4c8c19fe4a66aefa2cbe45d8
jakob <jakob.willforss@hotmail.com>
2018-11-22 22:03:40 +0100

    Preparing for multi-grid setup

--- Assets/Scripts/GridScript.cs
+++ Assets/Scripts/GridScript.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GridLanguage {
+    character,
+    pinying,
+    english
+}
+
 public class GridScript : MonoBehaviour {
 
     public GameObject mCellPrefab;
     public int cells;
 
-    private Cell[] mCells = new Cell[1000];
+    public GridLanguage cell_type;
 
-    public AnkiParser anki_parser;
+    private Cell[] mCells = new Cell[1000];
+    private AnkiParser anki_parser;
 
     void Awake() {
         anki_parser = GameObject.FindObjectOfType<AnkiParser>();
@@ -23,7 +30,6 @@ public class GridScript : MonoBehaviour {
         for (int i = 0; i <= cells; i++) {
             GameObject newCell = Instantiate(mCellPrefab, transform);
             mCells[i] = newCell.GetComponent<Cell>();
-
 
             string new_word = anki_parser.GetWord(i);
 
