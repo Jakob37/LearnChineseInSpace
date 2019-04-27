b91ee9a507c08cbe6f1df71126a45c324533f82f
jakob <jakob.willforss@hotmail.com>
2019-04-27 18:29:05 +0200

    Setting up basic test structure

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -2,18 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+    None
+}
+
 public class Test : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    private Transform transform;
+    private Movement movement;
+
+    void Start() {
+        movement = GetComponent<Movement>();
+        transform = gameObject.transform;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        //if (Input.anyKeyDown) {
+        //    transform.position = new Vector2(transform.position.x + 0.1f, transform.position.y);
+        //}
+
+        Vector2 dir = GetDirection();
+        if (dir != Vector2.zero) {
+            Move(dir);
+        }
+    }
+
+    private Vector2 GetDirection() {
+
+        Vector2 dir = new Vector2(0, 0);
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            dir = new Vector2(-1, dir.y);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            dir = new Vector2(1, dir.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            dir = new Vector2(dir.x, 1);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            dir = new Vector2(dir.x, -1);
+        }
+
+        return dir;
+    }
+
+    private void Move(Vector2 dir) {
+        print("Move triggered");
+        print(transform.position);
+
+        float speed = 50;
+
+        transform.position = new Vector2(transform.position.x + dir.x * speed, transform.position.y + dir.y * speed);
+
     }
 
     void OnMouseDown() {
