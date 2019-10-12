73427012947d23af297b3a78793fe21d10ba1d08
jakob <jakob.willforss@hotmail.com>
2019-10-12 15:05:00 +0200

    Basic functionality for end screen up

---
+++ Assets/FailedCharacterEntry.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FailedCharacterEntry : MonoBehaviour
+{
+    public bool destroy_on_load;
+
+    private string display_text;
+    private Text text;
+
+    void Awake() {
+        text = GetComponent<Text>();
+    }
+
+    public void SetText(string new_string) {
+        display_text = new_string;
+        this.text.text = new_string;
+    }
+}
---
+++ Assets/FailedCharactersDisplay.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FailedCharactersDisplay : MonoBehaviour
+{
+    public GameObject template_object;
+
+    void Start() {
+        print("Score: " + PlayerStats.score);
+        if (PlayerStats.incorr_chars != null) {
+            foreach (string character in PlayerStats.incorr_chars) {
+                print("You missed on: " + character);
+                CreateEntry(character);
+            }
+        }
+
+        CreateEntry("test1");
+        CreateEntry("test2");
+    }
+
+    private void CreateEntry(string text) {
+        GameObject instance = Instantiate(template_object);
+        instance.transform.SetParent(gameObject.transform);
+        FailedCharacterEntry menu_char = instance.GetComponent<FailedCharacterEntry>();
+        menu_char.SetText(text);
+        // menu_char.SetRadical(key);
+        // menu_char.SetCount(radical_dict[key], largest_count);
+        // menu_char.SetMostAbundantCount(largest_count);
+    }
+
+    void Update() {
+        
+    }
+}
